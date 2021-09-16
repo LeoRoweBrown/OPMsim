attempting to fix xy rotation and flat pupil

dipole.py
@@ -7,15 +7,14 @@ from scipy.interpolate import griddata
 
 class Dipole:
     def __init__(
-        self, phi, theta, lda_em=None, lda_exc=None) -> None:
+        self, phi_d, alpha_d, lda_em=None, lda_exc=None) -> None:
         """dipole class to simulate a dipole emitter
         Note: everything is in radians, but the optical system is in degrees
         Is this confusing?
         Arguments:
-            phi <float> -- azimuthal angle, rotation about z axis [rad]
+            phi_d <float> -- azimuthal angle, rotation about z axis [rad]
                            clockwise when looking along positive z (into paper)
-            theta <float> -- angle of dipole from x axis (note that ray calculations
-                     use a different convention - measuring theta from z) [rad].
+            alpha_d <float> -- angle of dipole from x axis [rad].
             lda_em <float> -- emission of fluorophore/dipole 
                               (to be implemented) [nm]
             lda_exc <float> -- excitaiton of fluorophore/dipole (used)
@@ -26,16 +25,20 @@ class Dipole:
         if lda_exc is None:
             lda_exc = 500e-9  # should be a distribution at some point
 
-        self.theta = (theta + np.pi/2) % (2*np.pi)  # 'theta' for rays (from x axis)
-        self.theta_dipole_coords = theta  # 'theta' definition for dipole (from y axis)
-        self.phi = phi
+        # self.theta = (theta + np.pi/2) % (2*np.pi)  # 'theta' for rays (from x axis)
+        self.alpha_d = alpha_d
+        # self.theta_dipole_coords = theta  # 'theta' definition for dipole (from y axis)
+        self.phi_d = phi_d
         self.lda_em = lda_em
         self.lda_exc = lda_exc
+        # expression of p has polar angle measured from z not x
+        alpha_90deg = (alpha_d + np.pi/2) % (2*np.pi)
 
         # get dipole angle
         # angles given with theta measured from x not z so cos(theta) <-> sin(theta)
-        self.p_vec = [ np.cos(theta)*np.cos(phi), np.cos(theta)*np.sin(phi), \
-            np.sin(theta)]
+        self.p_vec = [ np.cos(alpha_d)*np.cos(phi_d),\
+            np.cos(alpha_d)*np.sin(phi_d), \
+            np.sin(alpha_d)]
     
     def getEfield_z(self, theta, phi, z=1, dx=0, dy=0, dz=0):
         """ PROBABLY WONT BE USED 
@@ -71,10 +74,12 @@ class Dipole:
 
 
     def getEfield(self, theta, phi, r, curved_coords=True) -> np.ndarray:
-        """propagate the E-field along r for an angle (theta_p, phi_p) in
+        """propagate the E-field along r for an angle (theta, phi) in
            the pupil coordinates (?).
            Curved coords evaluates field perpendicular to ray and tangential to
-           curved surface of pupil
+           curved surface of pupil - NOTE: x and y basis is also rotated, so
+           when evalulating polarisation convert back with 
+           get_polarisation_xy_basis
         """
         # E = (e^ikr/r)k^2(n x p) x n
         # n = [sin(theta_p)cos(phi) i, sin(theta_p)sin(phi_p) j, cos(theta_p) k]
@@ -94,10 +99,17 @@ class Dipole:
                 print("Ez =", E_vec[2], "Ex =", E_vec[0], "E_y =", E_vec[1])
                 raise Exception("E_z is not zero in ray's frame!")
 
+            # now convert x and y rotated basis back to lab basis for meaningful
+            # polarisation
+            E_vec_x = E_vec[0]*np.cos(phi) + E_vec[1]*np.sin(phi)
+            E_vec_y = -E_vec[0]*np.sin(phi) + E_vec[1]*np.cos(phi)
+            E_vec[0] = E_vec_x
+            E_vec[1] = E_vec_y
+
         return (E_vec, E_mag, n_vec)
     
     def _rotate_efield(self, E_vec, theta_polar, phi):
-
+        """ changes coordinate system according to k vector so that Ez = 0 """
         E_x_tf = E_vec[0]*np.cos(phi)*np.cos(theta_polar) + \
             E_vec[1]*np.sin(phi)*np.cos(theta_polar)\
             - E_vec[2]*np.sin(theta_polar)
@@ -108,23 +120,6 @@ class Dipole:
         E_rot = [E_x_tf, E_y_tf, E_z_tf]
 
         return E_rot
-        """
-        gamma = np.arcsin(np.sin(theta_polar)*np.sin(phi))
-        theta = np.arcsin(np.sin(theta_polar)*np.cos(phi))
-        E_x_tf = E_vec[0]*np.cos(phi)*np.cos(theta) +\
-            E_vec[1]*np.sin(phi)*np.cos(theta) -\
-            E_vec[2]*np.sin(theta)
-        E_y_tf = E_vec[0]*(np.cos(phi)*np.sin(theta)*np.sin(gamma) - np.sin(phi)*np.cos(gamma)) +\
-            E_vec[1]*(np.sin(phi)*np.sin(theta)*np.sin(gamma) + np.cos(phi)*np.cos(gamma)) +\
-            E_vec[2]*(np.cos(theta)*np.sin(gamma))
-        # E_z_tf should equal 0
-        E_z_tf = E_vec[0]*(np.cos(phi)*np.sin(theta)*np.cos(gamma) + np.sin(phi)*np.sin(gamma)) +\
-            E_vec[1]*(np.sin(phi)*np.sin(theta)*np.cos(gamma) - np.cos(phi)*np.sin(gamma)) +\
-            E_vec[2]*np.cos(theta)*np.cos(gamma) 
-        E_rot = [E_x_tf, E_y_tf, E_z_tf]
-
-        return E_rot
-        """
 
     def new_ray(self, theta, phi, z):
         """calculate new E-field based on position in pupil at z defined by (theta, phi) and dipole position (dx dy dz)"""
@@ -209,13 +204,13 @@ class Dipole:
 class ComplexDipole(Dipole):
     """Includes beta, time correlation/lifetime, spectral fluoresence"""
     def __init__(
-        self, phi, theta, lda_em=None, lda_exc=None, beta=0, lifetime=None,
+        self, phi_d, alpha_d, lda_em=None, lda_exc=None, beta=0, lifetime=None,
         correlation=None) -> None:
         """dipole class to simulate a dipole emitter
         Arguments:
-            phi <float> -- azimuthal angle, rotation about z axis [rad]
+            phi_d <float> -- azimuthal angle, rotation about z axis [rad]
                            clockwise when looking along positive z (into paper)
-            theta <float> -- angle of dipole from x axis (note that ray calculations
+            alpha_d <float> -- angle of dipole from x axis (note that ray calculations
                      use a different convention - measuring theta from z) [rad].
             lda_em <float> -- emission of fluorophore/dipole 
                               (to be implemented) [nm]
@@ -245,23 +240,26 @@ class ComplexDipole(Dipole):
         # theta_=0 is parallel to optical axis z
 
         # non parallel exc and em dipoles
-        theta = theta + beta
+        self.alpha_exc = alpha_d
+        alpha = alpha_d + beta
 
-        self.theta_dipole_coords = theta  # 'theta' definition for dipole (from y axis)
-        # calculate new theta after diffusion #
+        self.alpha = alpha  # 'theta' definition for dipole (from y axis)
+        # calculate new alpha after diffusion (random sample this? decay stats?) #
         self.depolarise()
 
         # now convert these to the system coordinates
-        self.theta = (theta + np.pi/2) % (2*np.pi)  # 'theta' for rays (from z axis)
+        self.alpha = (alpha_d) % (2*np.pi)  # 'theta' for rays (from z axis)
+        alpha_90deg = (alpha_d + np.pi/2) % (2*np.pi)
         
-        self.phi = phi
+        self.phi_d = phi_d
         self.lda_em = lda_em
         self.lda_exc = lda_exc
 
         # get dipole angle
         # angles given with theta measured from x not z so cos(theta) <-> sin(theta)
-        self.p_vec = [ np.cos(theta)*np.cos(phi), np.cos(theta)*np.sin(phi), \
-            np.sin(theta)]
+        self.p_vec = [ np.cos(alpha_d)*np.cos(phi_d),\
+            np.cos(alpha_d)*np.sin(phi_d), \
+            np.sin(alpha_d)]
 
     def depolarise(self, direction=None):
         """only considers rotation in one dimension (for now? okay who am I kidding)

plot_single_dipole.py
@@ -9,10 +9,10 @@ import os
 
 def plot_single_dipole(angles, data, dipole_angles, save_dir = None):
     pupil_phi_range, pupil_sin_theta_range = angles
-    dipole_phi, dipole_theta = dipole_angles
+    dipole_phi, dipole_alpha = dipole_angles
     data_intensity_x, data_intensity_y = data
     data_intensity_sum = data_intensity_x + data_intensity_y
-    dipole_theta_from_y = (dipole_theta - np.pi/2) % (2*np.pi)
+    # dipole_theta_from_y = (dipole_theta - np.pi/2) % (2*np.pi)
 
     fig = plt.figure(figsize=[10,4])
     #Create a polar projection
@@ -30,7 +30,7 @@ def plot_single_dipole(angles, data, dipole_angles, save_dir = None):
     # ax1.pcolormesh(pupil_phi_range,pupil_sin_theta_range,pupil_vals_x, shading='auto')
 
     # dipole arrow length depends on theta?
-    arrow_len = np.abs(0.5*np.sin(dipole_theta))
+    arrow_len = np.abs(0.5*np.cos(dipole_alpha))
 
     flip_phi = (dipole_phi+np.pi) % (2*np.pi)
     # p1 = patches.FancyArrowPatch((flip_phi, arrow_len), (dipole_phi, arrow_len), arrowstyle='<->',\
@@ -67,12 +67,12 @@ def plot_single_dipole(angles, data, dipole_angles, save_dir = None):
     ax3.set_title("X+Y polarisation pupil field distribution")
 
     dipole_phi_deg = dipole_phi*180/np.pi
-    dipole_theta_from_y_deg = dipole_theta_from_y*180/np.pi
+    dipole_alpha_deg = dipole_alpha*180/np.pi
 
     # ax2.set_ylim([0, 1])
     fig.suptitle("Single dipole radiation in pupil for "\
-        + "$\\phi = %.1f^\circ, \\theta_d = %.1f^\circ$" %\
-            (dipole_phi_deg, dipole_theta_from_y_deg))
+        + "$\\phi_d = %.1f^\circ, \\alpha_d = %.1f^\circ$" %\
+            (dipole_phi_deg, dipole_alpha_deg))
 
     fig.tight_layout()
     if save_dir is None:
@@ -81,12 +81,12 @@ def plot_single_dipole(angles, data, dipole_angles, save_dir = None):
         save_dir = os.path.normpath(save_dir)
         print("Saving in %s" % save_dir)
         save_dir_theta = os.path.join(save_dir, 'theta_dipole_%.1f' \
-            % dipole_theta_from_y_deg)
+            % dipole_alpha_deg)
         if not os.path.isdir(save_dir_theta):
             print("No directory, %s, making it" % save_dir_theta)
             os.makedirs(save_dir_theta)
         full_save_path = os.path.join(save_dir_theta, \
-            'single_dipole_theta_%.1f_phi_%.1f.png' %\
-            (dipole_theta_from_y_deg, dipole_phi_deg))
+            'single_dipole_alpha_%.1f_phi_%.1f.png' %\
+            (dipole_alpha_deg, dipole_phi_deg))
         fig.savefig(full_save_path, bbox_inches='tight')
     plt.close(fig)

single_dipole.py
@@ -7,11 +7,12 @@ import dipole as dipole
 from plot_single_dipole import plot_single_dipole
 from tools import printif  # is the name tools a little dangerous? conflicts etc
 
-def simulate_single_dipole(dipole_phi, dipole_theta, NA, dipole_obj=None, plot_pupil=None,
+def simulate_single_dipole(dipole_phi, dipole_alpha, NA, flat_pupil=False,
+          dipole_obj=None, plot_pupil=None,
           save_dir=None, show_prints=False):
 
      if dipole_obj is None:
-          single_dipole = dipole.Dipole(dipole_phi, dipole_theta) # phi then theta
+          single_dipole = dipole.Dipole(dipole_phi, dipole_alpha) # phi then theta
      elif isinstance(dipole_obj, dipole.Dipole):
           single_dipole = dipole_obj
      else:
@@ -23,8 +24,8 @@ def simulate_single_dipole(dipole_phi, dipole_theta, NA, dipole_obj=None, plot_p
      pupil_theta_range = np.arcsin(pupil_sin_theta_range)
      pupil_phi_range = np.linspace(0,2*np.pi,100)
 
-     printif('dipole angle: phi = %f theta = %f' % (single_dipole.phi*(180/np.pi),\
-          single_dipole.theta_dipole_coords*(180/np.pi)), show_prints)
+     printif('dipole angle: phi = %f theta = %f' % (single_dipole.phi_d*(180/np.pi),\
+          single_dipole.alpha_d*(180/np.pi)), show_prints)
 
      pupil_vals_x = np.zeros([len(pupil_theta_range), len(pupil_phi_range)])
      pupil_vals_y = np.zeros([len(pupil_theta_range), len(pupil_phi_range)])
@@ -38,7 +39,13 @@ def simulate_single_dipole(dipole_phi, dipole_theta, NA, dipole_obj=None, plot_p
      i = 0
      for t_i, theta in enumerate(pupil_theta_range):
           for p_i, phi in enumerate(pupil_phi_range):
-               e_vec, e_mag, k_vec = single_dipole.getEfield(theta, phi, 1)
+               e_vec = np.array([])
+               e_mag = np.array([])
+               k_vec = np.array([])
+               if flat_pupil:
+                    e_vec, e_mag, k_vec = single_dipole.getEfield_z(theta, phi, 1)
+               else:
+                    e_vec, e_mag, k_vec = single_dipole.getEfield(theta, phi, 1)
                # print(e_vec)
                # print('######')
                pupil_vals_x[t_i, p_i] = e_vec[0]
@@ -76,6 +83,6 @@ def simulate_single_dipole(dipole_phi, dipole_theta, NA, dipole_obj=None, plot_p
           printif("Plotting...", show_prints)
           plot_single_dipole((pupil_phi_range,pupil_sin_theta_range),\
                (data_intensity_x, data_intensity_y),\
-               (single_dipole.phi, single_dipole.theta), save_dir)
+               (single_dipole.phi_d, single_dipole.alpha_d), save_dir)
 
      return points, vals_efield_x, vals_efield_y